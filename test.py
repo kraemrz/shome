5be2fc45f02da7a11179d3c03f9ee0ed580b4881
Björn Lindstrand <32561285+kraemrz@users.noreply.github.com>
2019-03-30 21:35:14 +0100

    2019-03-30 21:34
    added in GPIO in the test file to be able to try if code work

--- test.py
+++ test.py
@@ -1,4 +1,36 @@
+import RPi.GPIO as GPIO
+
+
+# Sätter hur den ska läsa pin
+GPIO.setmode(GPIO.BOARD)
+# Sätter vilka pins och om det I eller O'
+btn = GPIO.setup(16, GPIO.IN)
+relay = GPIO.setup(15, GPIO.OUT)
+
+
+# connect button to GPIO BOARD 16, relay to GPIO BOARD 15,
+
+
 def main():
+    # might be able to read relay signal instead
+    pwr = 0
+    # run loop cheking if btn is pressed
+    while True:
+        # listening for a button press
+        if btn == 1:
+            # check if power is on or off
+            if pwr == 0:
+                # if power is off, we turn it on here
+                print('The power is off,\n')
+                pwr = 1
+                print('The power is on\n')
+            else:
+                # if power is on, we turn it on here
+                print('The power is on,\n')
+                pwr = 0
+                print('The power is off\m')
+
+    '''
     while True:
         button = 0
         power = 0
@@ -11,14 +43,8 @@ def main():
                 powerOn()
             elif power == '1':
                 powerOff()
+'''
 
-
-def powerOn():
-    print('The power is now ON')
-
-
-def powerOff():
-    print ('The power is now OFF')
 
 if __name__ == '__main__':
     main()
